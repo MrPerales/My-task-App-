403fd00cd28ef9be82665b36edac68510b9cd3f9
Carlos Perales <carlosperales050@gmail.com>
2023-04-12 20:55:25 -0600

    prueba de React.useState en archivo TodoSearch.js

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -2,22 +2,29 @@ import React from "react";
 import '../Style-Components/todoSearch.css';
 
 function TodoSearch(){
+
+    const [searchValue,setSearchValue]= React.useState('');
+
     //  methods for onChange 
     const onSearchValueChange=(event)=>{
         console.log(event.target.value);
+        setSearchValue(event.target.value)
     }
     
-    return(
+    return[
         <div className="input">
             <input 
                 className="inputSearch"
                 placeholder="search task"
+                value={searchValue}
                 onChange={onSearchValueChange}
             >
             </input>
             {/* <button className="searchBtn">🔍</button> */}
-        </div>
-    );
+        </div>,
+        
+        <p>{searchValue}</p>
+    ];
 }
 
 export {TodoSearch};
